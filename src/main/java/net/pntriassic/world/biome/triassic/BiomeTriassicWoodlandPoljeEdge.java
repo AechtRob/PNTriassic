e269a1a5fd0a17f0e6840530216ebb89939c76dd
AechtRob <aecht_rob@outlook.com>
2024-04-02 19:39:12 +0100

    New plants (v.60 main mod)

--- src/main/java/net/pntriassic/world/biome/triassic/BiomeTriassicWoodlandPoljeEdge.java
+++ src/main/java/net/pntriassic/world/biome/triassic/BiomeTriassicWoodlandPoljeEdge.java
@@ -286,6 +286,15 @@ public class BiomeTriassicWoodlandPoljeEdge extends ElementsLepidodendronMod.Mod
 					IRANIA_GENERATOR.generate(worldIn, rand, pos.add(j, l, k));
 				}
 
+			if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, new net.minecraft.util.math.ChunkPos(pos), net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.GRASS))
+				for (int i = 0; i < 48; ++i)
+				{
+					int j = rand.nextInt(16) + 8;
+					int k = rand.nextInt(16) + 8;
+					int l = rand.nextInt(worldIn.getHeight(pos.add(j, 0, k)).getY() + 32);
+					PLANT_GENERATOR.generate(BlockSphenopteris.block.getDefaultState(), worldIn, rand, pos.add(j, l, k), 0, 75);
+				}
+
 //			if(net.minecraftforge.event.terraingen.TerrainGen.decorate(worldIn, rand, new net.minecraft.util.math.ChunkPos(pos), net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.GRASS))
 //				for (int i = 0; i < 6; ++i)
 //				{
